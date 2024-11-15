570b689a45405bab6aa7ca7528b2aad25df55d42
junjie <wangjunjie0817@foxmail.com>
2024-11-15 18:11:25 +0800

    ci: add generate action

---
+++ scripts/git.ts
@@ -0,0 +1,54 @@
+import { execSync } from "child_process";
+
+export async function isNeedPush() {
+  const stdout = await execSync("git status -s", {
+    encoding: "utf-8",
+  });
+  return !!stdout;
+}
+
+export async function add() {
+  const stdout = await execSync("git add -A .", {
+    encoding: "utf-8",
+  });
+  return stdout;
+}
+
+/**
+ * @param {string} commitMessage
+ */
+export async function commit(commitMessage) {
+  await execSync(`git config --local user.email "actions@github.com"`, {
+    encoding: "utf-8",
+  });
+  await execSync(`git config --local user.name "Github Actions"`, {
+    encoding: "utf-8",
+  });
+  await execSync(`git commit -m "${commitMessage}"`, {
+    encoding: "utf-8",
+  });
+}
+
+/**
+ * @param {string} branch
+ */
+export async function push(branch) {
+  await execSync(`git push --set-upstream origin ${branch}`, {
+    encoding: "utf-8",
+  });
+}
+
+async function main() {
+  const needPush = await isNeedPush();
+  if (needPush) {
+    if (!process.env.COMMIT_MESSAGE) {
+      throw new Error("process.env.COMMIT_MESSAGE is required");
+    }
+    await add();
+    await commit(process.env.COMMIT_MESSAGE);
+  } else {
+    console.log("nothing to commit, working tree clean");
+  }
+}
+
+main();
